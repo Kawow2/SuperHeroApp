e5f5c0d2cf4171ce7e59664c8172878732e24a8d
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2022-09-05 00:00:34 +0200

    Initial commit

---
+++ SuperHeroAPI/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>
+            {
+                new SuperHero
+                {
+                     FirstName = "Peter",
+                     LastName = "Parker",
+                     Name = "SpiderMan",
+                     Place = "NYC",
+                }
+            };
+        }
+
+    }
+}
